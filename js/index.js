a2758dc71c694f4ab956f3c05f59e2a2773559b0
DESKTOP-J2DM1UE\Michael <michaelbarnes7282>
2020-03-10 12:40:54 -0700

    added 3 events

--- js/index.js
+++ js/index.js
@@ -1,1 +1,22 @@
 // Your code goes here
+//Change boom button to red
+boomBtn.setAttribute('style', 'background-color: red');
+
+//When boombtn is clicked, remove home and replace nav
+var container = document.querySelector('.home');
+boomBtn.addEventListener('click', () => {
+    container.style.display = 'none';
+    document.querySelector('.nav').innerHTML = '<h2>I Told You Not To</h2>';
+})
+
+// When Mouseover Nav, body turns red
+var links = document.querySelector('.nav');
+links.addEventListener('mouseover', () => {
+    document.body.style.backgroundColor = 'red';
+})
+
+// When mouseleave body goes back to white
+links.addEventListener('mouseleave', () => {
+    document.body.style.backgroundColor = 'white';
+})
+
