9dfcbe91aae83ee0a3a1adcd3c5054ac399293ad
DESKTOP-J2DM1UE\Michael <michaelbarnes7282>
2020-03-10 17:33:11 -0700

    finsished the 10 events

--- js/index.js
+++ js/index.js
@@ -9,6 +9,17 @@ boomBtn.addEventListener('click', () => {
     document.querySelector('.nav').innerHTML = '<h2>I Told You Not To</h2>';
 })
 
+// Warn user to not press button on hover
+var output = document.querySelector('#output p')
+boomBtn.addEventListener('mouseenter', () => {
+    output.textContent = "Don't do it!";
+})
+
+// Final warning on initial press
+boomBtn.addEventListener('mousedown', () => {
+    output.textContent = "I'm warning you!";
+})
+
 // When Mouseover Nav, body turns red
 var links = document.querySelector('.nav');
 links.addEventListener('mouseover', () => {
@@ -28,6 +39,23 @@ img.addEventListener('dblclick', (e) => {
 
 // When copying this h2 to clipboard, change that h2
 var h2 = document.querySelector('.intro h2');
-h2.addEventListener('copy', () =>{
+h2.addEventListener('copy', () => {
     h2.textContent = 'Please do not copy me :(';
+})
+
+//Body text will change color on wheel
+var p = document.querySelector('.destination p');
+document.body.addEventListener('wheel', () => {
+    document.body.style.color = `rgb(${Math.floor(Math.random() * 255)}, 
+    ${Math.floor(Math.random() * 255)}, 
+    ${Math.floor(Math.random() * 255)})`;
+})
+
+var imgs = document.querySelector('.content-destination img');
+document.body.addEventListener('keydown', () => {
+    imgs.style.display = 'none';
+})
+
+document.body.addEventListener('keyup', () => {
+    imgs.style.display = 'block';
 })
